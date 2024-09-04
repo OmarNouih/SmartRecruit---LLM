204975b8e90801231b9a0f0fc89c1eb987bc07eb
OmarNouih <omarnouih@gmail.com>
2024-09-04 11:16:44 +0100

    Set up environment variables with .env file

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv # type: ignore
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'TESTINGCHEATS123'
@@ -7,6 +10,6 @@ class Config:
     SESSION_TYPE = 'filesystem'
     UPLOAD_FOLDER_CV = os.path.join('app', 'static', 'uploads', 'cv')
     UPLOAD_FOLDER_PHOTOS = os.path.join('app', 'static', 'uploads', 'photos')
-    API_TOKEN = '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'
+    API_TOKEN = os.environ.get('API_TOKEN', 'default_api_token')
     API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct"
     MONGO_URI = 'mongodb://localhost:27017/applications'
